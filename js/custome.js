cb2bdf865ffe9c155ad2751681f7d85f216c732c
pintu <paulpintu858@gmail.com>
2025-12-17 22:21:17 +0530

    all

---
+++ js/custome.js
@@ -0,0 +1,8 @@
+//////////////toggle-header//////////////
+$(".toggle_button button").on("click", function(){
+    $(".manu").addClass("active");
+});
+$(".header_clo_button").on("click", function(){
+    $(".manu").removeClass("active");
+});
+//////////////toggle-header//////////////
